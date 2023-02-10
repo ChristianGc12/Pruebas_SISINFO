bb0c7194cdd22ad5cbc47a1fd5954e805ae77db3
Asus <Asus@LAPTOP-58FTF0VJ>
2023-02-10 16:39:23 +0100

    Prints

--- src/clase.java
+++ src/clase.java
@@ -3,7 +3,19 @@ public class clase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("ejemplo");
+		System.out.print("ejemplo");
+		System.out.print("ejemplo");
+		System.out.print("ejemplo");
+
+		
+		System.out.print("ejemplo");
+		System.out.print("ejemplo2");
+		System.out.print("ejemplo3");
+		System.out.print("ejemplo4");
+
+		System.out.print("ejemplo");
+
+
 	}
 
 }
